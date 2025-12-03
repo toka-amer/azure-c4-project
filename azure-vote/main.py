0e2a43db945947f698457c2ec3c5a8071916acb5
Toka Amer <64048448+toka-amer@users.noreply.github.com>
2025-12-03 04:30:06 +0200

    Comment out local app.run and enable deployment run

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -148,7 +148,7 @@ def index():
 
 if __name__ == "__main__":
     # LOCAL
-    app.run()
+    #app.run()
 
     # BEFORE DEPLOYING
-    # app.run(host="0.0.0.0", threaded=True, debug=True)
+    app.run(host="0.0.0.0", threaded=True, debug=True)

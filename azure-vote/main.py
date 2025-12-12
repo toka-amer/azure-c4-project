f914c17eae8f9588a33ddbb7e7cb9a175b459b85
Toka Amer <64048448+toka-amer@users.noreply.github.com>
2025-12-12 03:16:54 +0200

    Implement logging for cat and dog votes
    Added logging for cat and dog votes using the logger object.

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -177,11 +177,13 @@ def index():
             # Get current values
             vote1 = r.get(button1).decode("utf-8")
             properties = {"custom_dimensions": {"Cats Vote": vote1}}
-            
+            # TODO: use logger object to log cat vote
+            logger.info("Cats Vote", extra=properties)
 
             vote2 = r.get(button2).decode("utf-8")
             properties = {"custom_dimensions": {"Dogs Vote": vote2}}
-            
+            # TODO: use logger object to log dog vote
+            logger.info("Dogs Vote", extra=properties)            
 
             # Return results
             return render_template(
